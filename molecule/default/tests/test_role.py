a943951f01b39e4ecff6a6d62b031ccb8e9e256c
Nicolas Boutet <amd3002@gmail.com>
2023-04-27 18:06:45 +0200

    Adapt CI workflow for molecule 5

--- molecule/default/tests/test_role.py
+++ molecule/default/tests/test_role.py
@@ -5,20 +5,27 @@ import os
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
-    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+    os.environ["MOLECULE_INVENTORY_FILE"]
+).get_hosts("all")
 
 
-@pytest.mark.parametrize('name', [
-  ('filebeat'),
-])
+@pytest.mark.parametrize(
+    "name",
+    [
+        ("filebeat"),
+    ],
+)
 def test_packages_are_installed(host, name):
     package = host.package(name)
     assert package.is_installed
 
 
-@pytest.mark.parametrize('username,groupname,path', [
-  ('root', 'root', '/etc/filebeat/filebeat.yml'),
-])
+@pytest.mark.parametrize(
+    "username,groupname,path",
+    [
+        ("root", "root", "/etc/filebeat/filebeat.yml"),
+    ],
+)
 def test_filebeat_config_file(host, username, groupname, path):
     config = host.file(path)
     assert config.exists
